ebf8fd0c34af30148f71562a52f85ed8261d640b
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-01-19 20:35:58 -0500

    within if statment, include criteria - uppercase, lowercase, numbers, symbols - else statment alert function

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,14 @@
-// Assignment Code
+// Assignment code here
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 var upperCase= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-var lowerCase= 'abcdefghijklmnopqrstuvwxyz'
+var lowerCase= 'abcdefghijklmnopqrstyuvwxyz'
 var symbols= '!@#$%^&*()_+='
 var numbers= '0123456789'
 
-
-function generatePassword () {
-let passwordLength = prompt ('What is the Length of the Password? No less than 8 and No More 128 Characters ')
-if (passwordLength <128 && passwordLength >8)
-
-}
-
+let userPassword = []
 
 // Write password to the #password input
 function writePassword() {
@@ -23,6 +17,29 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+
+function generatePassword () {
+  let passwordLength =  prompt('What is the Length of the Password? No less than 8 and No More 128 Characters ')
+  if(passwordLength < 128 && passwordLength > 8){
+
+    let upperChoice = confirm('Would you like uppercase?')
+
+    let lowerChoice = confirm('Would you like lowercase?')
+    
+    let numberChoice = confirm('Would you like numbers?')
+    
+    let symbolsChoice = confirm('Would you like symbols?')
+
+
+
+
+  }else{
+    alert('Please and Kindly Follow the Instructions')
+  }
+
+  // return finalpassword
 }
 
 // Add event listener to generate button
