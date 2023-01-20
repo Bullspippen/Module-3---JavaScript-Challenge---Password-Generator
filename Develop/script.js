a496263d6f6b032e9e74029097ed04af60d0c656
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-01-19 20:09:59 -0500

    declared variables on script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,14 @@
-// Assignment code here
+// Assignment Code
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var upperCase= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var lowerCase= 'abcdefghijklmnopqrstuvwxyz'
+var symbols= '!@#$%^&*()_+='
+var numbers= '0123456789'
+
+
 
 // Write password to the #password input
 function writePassword() {
