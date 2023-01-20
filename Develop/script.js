20a77f09a58fd6e69e1ebeb2acb06088ab2f5517
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-01-19 20:42:54 -0500

    if statements added to criteria

--- Develop/script.js
+++ Develop/script.js
@@ -19,20 +19,27 @@ function writePassword() {
 
 }
 
-
+// Match Criteria
 function generatePassword () {
   let passwordLength =  prompt('What is the Length of the Password? No less than 8 and No More 128 Characters ')
   if(passwordLength < 128 && passwordLength > 8){
 
     let upperChoice = confirm('Would you like uppercase?')
-
+    if(upperChoice){
+      userPassword.push(...upperCase)
+    }
     let lowerChoice = confirm('Would you like lowercase?')
-    
+    if(lowerChoice){
+      userPassword.push(...lowerCase)
+    }
     let numberChoice = confirm('Would you like numbers?')
-    
+    if(numberChoice){
+      userPassword.push(...numbers)
+    }
     let symbolsChoice = confirm('Would you like symbols?')
-
-
+    if(symbols){
+      userPassword.push(...symbols)
+    }
 
 
   }else{
