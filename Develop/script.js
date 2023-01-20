19aa90137b15a08c5809a66eed42e29ec1a7c48c
Mohamed Osman <mohamedosman@Mohameds-MacBook-Pro.local>
2023-01-19 21:40:11 -0500

    randomized password, output established

--- Develop/script.js
+++ Develop/script.js
@@ -23,30 +23,38 @@ function writePassword() {
 function generatePassword () {
   let passwordLength =  prompt('What is the Length of the Password? No less than 8 and No More 128 Characters ')
   if(passwordLength < 128 && passwordLength > 8){
-
-    let upperChoice = confirm('Would you like uppercase?')
+    let upperChoice = confirm('Would you like Uppercase Characters?')
     if(upperChoice){
       userPassword.push(...upperCase)
+    } else{
+      alert('hello')
     }
-    let lowerChoice = confirm('Would you like lowercase?')
+    let lowerChoice = confirm('Would you like Lowercase Characters?')
     if(lowerChoice){
       userPassword.push(...lowerCase)
     }
-    let numberChoice = confirm('Would you like numbers?')
+    let numberChoice = confirm('Would you like Numbers?')
     if(numberChoice){
       userPassword.push(...numbers)
     }
-    let symbolsChoice = confirm('Would you like symbols?')
+    let symbolsChoice = confirm('Would you like Symbols?')
     if(symbols){
       userPassword.push(...symbols)
     }
 
 
+
   }else{
     alert('Please and Kindly Follow the Instructions')
   }
-
-  // return finalpassword
+  console.log(userPassword)
+  var password=''
+  for (let i = 0; i < passwordLength; i++) {
+    password+=userPassword[Math.floor(Math.random() * userPassword.length)]
+  }
+  console.log(password)
+  
+  return password
 }
 
 // Add event listener to generate button
